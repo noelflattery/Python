3fd37582de21968d71a489d4993c660b9f8d5211
noelflattery <noelflattery@gmail.com>
2022-10-14 11:59:37 +0100

    first commit

---
+++ Loops.py
@@ -0,0 +1,5 @@
+i = 1
+while i < 12:
+  print("your number is ",i)
+  i += 1
+  
---
+++ hello.py
@@ -0,0 +1,12 @@
+msg = "Hello World"
+print(msg)
+
+#here is a comment
+#some more stuff
+num=34
+#more changes
+name="noel"
+#and yet another commit
+#put up this one
+car="mycar"
+address="Galway"
