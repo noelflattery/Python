34f1e038b703d360fc4b4fd22e107d648a07d8ef
noelflattery <noelflattery@gmail.com>
2022-10-14 12:01:10 +0100

    This is my first commit

--- hello.py
+++ hello.py
@@ -1,12 +1,4 @@
 msg = "Hello World"
 print(msg)
 
-#here is a comment
-#some more stuff
-num=34
-#more changes
-name="noel"
-#and yet another commit
-#put up this one
-car="mycar"
-address="Galway"
+#here is a comment to test our commit
